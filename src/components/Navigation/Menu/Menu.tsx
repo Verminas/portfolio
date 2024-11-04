b404e08021761ed897f7c4b30c3c03ccd48c5874
Verminas <veremeeva96@gmail.com>
2024-11-04 15:36:43 +0300

    feat: add styles for active navigation link

--- src/components/Navigation/Menu/Menu.tsx
+++ src/components/Navigation/Menu/Menu.tsx
@@ -2,7 +2,8 @@
 
 import { useTranslations } from "next-intl";
 import { navigationData } from "@/src/data/navigationData";
-import { Link } from "@/i18n/routing";
+import { Link, usePathname } from "@/i18n/routing";
+import styles from "./Menu.module.sass"
 
 type Props = {
   isOpenMenu?: boolean
@@ -10,13 +11,16 @@ type Props = {
 };
 export const Menu = ({isOpenMenu, setIsOpen}: Props) => {
   const t = useTranslations("Navigation");
+  const pathName = usePathname();
+  console.log(pathName)
 
   const onClickLink = () => {
     if(isOpenMenu && setIsOpen) setIsOpen(false)
   }
 
+
   const navigationElements = navigationData.map((item) => (
-    <Link href={item.href} key={item.title} onClick={onClickLink}>
+    <Link href={item.href} key={item.title} onClick={onClickLink} className={pathName === item.href ? styles.active : ''}>
       {t(item.title)}
     </Link>
   ));
